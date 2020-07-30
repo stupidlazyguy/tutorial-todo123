958e42c8efc91a1cd2fef25d640468ed37b75047
MK404xL <shinydark.sky@gmail.com>
2020-07-30 21:03:36 +0700

    Change port

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -17,7 +17,7 @@ const isLocalhost = Boolean(
     // 127.0.0.0/8 are considered localhost for IPv4.
     window.location.hostname.match(
       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
+    ) || process.env.PORT
 );
 
 export function register(config) {
